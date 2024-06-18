3c12bbef9c53a54ff1ae71ab8ec7a484aa5c5d08
Julia <veluat@gmail.com>
2024-06-18 23:34:05 +0300

    style: refactored all styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
-import {App} from './App';
-import {store} from './bll/store';
+import {App} from './app/App';
+import {store} from './app/reducer/store';
 import {Provider} from 'react-redux';
 
 const root = ReactDOM.createRoot(
