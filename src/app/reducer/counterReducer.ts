9f5b636f26bb085e013013db2a14dc0d49447720
Julia <veluat@gmail.com>
2024-06-19 16:32:48 +0300

    ref: add changeEditModeAC to Reducer

--- src/app/reducer/counterReducer.ts
+++ src/app/reducer/counterReducer.ts
@@ -1,42 +1,42 @@
 export type StateType = {
-    count: number,
-    maxValue: number,
-    startValue: number,
-    disabled: boolean
-    editMode: boolean
+  count: number
+  maxValue: number
+  startValue: number
+  disabled: boolean
+  isEditMode: boolean
 }
 
 export type ActionsType = ReturnType<typeof increaseCountAC>
-    | ReturnType<typeof resetCountAC>
-    | ReturnType<typeof changeStartValueAC>
-    | ReturnType<typeof changeMaxValueAC>
-    | ReturnType<typeof counterSettingsAC>
-    | ReturnType<typeof setCountFromLocalStorageAC>
-    | ReturnType<typeof changeEditModeAC>
+  | ReturnType<typeof resetCountAC>
+  | ReturnType<typeof changeStartValueAC>
+  | ReturnType<typeof changeMaxValueAC>
+  | ReturnType<typeof counterSettingsAC>
+  | ReturnType<typeof setCountFromLocalStorageAC>
+  | ReturnType<typeof changeEditModeAC>
 
-export const initialState: StateType = {count: 0, maxValue: 5, startValue: 0, disabled: true, editMode: true}
+export const initialState: StateType = {count: 0, maxValue: 5, startValue: 0, disabled: true, isEditMode: true}
 
 export const counterReducer = (state = initialState, action: ActionsType): StateType => {
-    switch (action.type) {
-        case 'INC-COUNT' :
-            return (state.count < state.maxValue && state.count >= state.startValue)
-                ? {...state, count: state.count + 1}
-                : state
-        case 'SET-COUNT' :
-            return {...state, count: action.value}
-        case 'RESET-COUNT' :
-            return {...state, count: state.startValue}
-        case 'CHANGE-START-VALUE' :
-            return {...state, startValue: Math.floor(action.start), disabled: false}
-        case 'CHANGE-MAX-VALUE' :
-            return {...state, maxValue: Math.floor(action.max), disabled: false}
-        case 'COUNTER-SETTINGS' :
-            return {...state, count: state.startValue, disabled: true}
-        case 'CHANGE-EDIT':
-            return {...state, editMode: !state.editMode}
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'INC-COUNT' :
+      return (state.count < state.maxValue && state.count >= state.startValue)
+        ? {...state, count: state.count + 1}
+        : state
+    case 'SET-COUNT' :
+      return {...state, count: action.value}
+    case 'RESET-COUNT' :
+      return {...state, count: state.startValue}
+    case 'CHANGE-START-VALUE' :
+      return {...state, startValue: Math.floor(action.start), disabled: false}
+    case 'CHANGE-MAX-VALUE' :
+      return {...state, maxValue: Math.floor(action.max), disabled: false}
+    case 'COUNTER-SETTINGS' :
+      return {...state, count: state.startValue, disabled: true}
+    case 'CHANGE-EDIT-MODE':
+      return {...state, isEditMode: !state.isEditMode}
+    default:
+      return state;
+  }
 }
 
 export const increaseCountAC = () => ({type: 'INC-COUNT'} as const)
@@ -45,4 +45,4 @@ export const resetCountAC = () => ({type: 'RESET-COUNT'} as const)
 export const changeStartValueAC = (start: number) => ({type: 'CHANGE-START-VALUE', start} as const)
 export const changeMaxValueAC = (max: number) => ({type: 'CHANGE-MAX-VALUE', max} as const)
 export const counterSettingsAC = () => ({type: 'COUNTER-SETTINGS'} as const)
-export const changeEditModeAC = () => ({type: 'CHANGE-EDIT'} as const)
+export const changeEditModeAC = (isEditMode: boolean) => ({type: 'CHANGE-EDIT-MODE', isEditMode} as const)
