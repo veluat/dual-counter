7dc19b5aa137f780ea0ebc1bfd8e615847829be0
Julia <veluat@gmail.com>
2024-06-20 23:43:38 +0300

    ref: refactored and optimized counter component

--- src/feature/counter/Counter.tsx
+++ src/feature/counter/Counter.tsx
@@ -1,63 +1,46 @@
 import React from 'react';
-import {CountDisplay} from "../../components/count-display/CountDisplay";
-import {Button} from "../../components/button/Button";
-import {ErrorType} from '../../app/App'
+import {CountDisplay} from "../../common/components";
+import {Button} from "../../common/components";
 import s from './Counter.module.scss'
-import {SettingsDisplay} from '../../components/settings-display/SettingsDisplay'
+import {SettingsDisplay} from '../../common/components'
+import {CounterPropsType} from '../../common/types'
 
-type CounterProps = {
-  count: number;
-  maxValue: number;
-  startValue: number;
-  disabled: boolean;
-  error: ErrorType;
-  counterSet: () => void;
-  changeStartValue: (start: number) => void
-  changeMaxValue: (max: number) => void
-  increaseCount: () => void
-  resetCount: () => void
-};
-export const Counter: React.FC<CounterProps> = ({
-                                                  count,
-                                                  maxValue,
-                                                  startValue,
-                                                  disabled,
-                                                  error,
-                                                  counterSet,
-                                                  changeStartValue,
-                                                  changeMaxValue,
-                                                  increaseCount,
-                                                  resetCount
-                                                }) => {
+export const Counter: React.FC<CounterPropsType> = ({
+                                                      triggerCounterSet,
+                                                      changeStartValue,
+                                                      changeMaxValue,
+                                                      increaseCount,
+                                                      resetCount,
+                                                      ...countState
+                                                    }) => {
 
 
   return (
     <div className={s.root}>
       <div className={s.container}>
-        <SettingsDisplay maxValue={maxValue} startValue={startValue}
+        <SettingsDisplay {...countState}
                          changeStartValue={changeStartValue}
                          changeMaxValue={changeMaxValue}
-                         error={error}
         />
         <div className={s.display_button}>
-          <Button name={'Set'}
-                  isDisabled={error.startValueError || error.maxValueError || disabled}
-                  counterSettings={counterSet}
+          <Button name={'set'}
+                  isDisabled={countState.error.startValueError || countState.error.maxValueError || countState.disabled}
+                  triggerCounterSet={triggerCounterSet}
           />
         </div>
       </div>
       <div className={s.container}>
-        <CountDisplay counter={count}
-                      max={maxValue}
-                      disabled={disabled}
-                      error={error}/>
+        <CountDisplay counter={countState.count} disabled={countState.disabled} error={countState.error}
+                      maxValue={countState.maxValue}/>
         <div className={s.display_button}>
-          <Button name={'Inc'}
-                  isDisabled={count === maxValue || !disabled}
-                  counterSettings={increaseCount}/>
-          <Button name={'Reset'}
-                  isDisabled={count === startValue || !disabled}
-                  counterSettings={resetCount}/>
+          <Button {...countState}
+                  name={'inc'}
+                  isDisabled={countState.count === countState.maxValue || !countState.disabled}
+                  triggerCounterSet={increaseCount}/>
+          <Button {...countState}
+                  name={'reset'}
+                  isDisabled={countState.count === countState.startValue || !countState.disabled}
+                  triggerCounterSet={resetCount}/>
         </div>
       </div>
     </div>
